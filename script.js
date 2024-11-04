220632a890f45250e580208afee403724f7d3675
Erkezhan <e1344935@gmail.com>
2024-11-04 14:32:45 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Курделі өрнек
+let age=20,hasTicket=true,isVip=false;
+
+if((age>=18 && hasTicket) || isVip){
+    console.log("сізге кешке кіруге болады!");
+
+}
+else{
+    console.log("сізге кешке кіруге тыйым салынады!");
+}
+//Нәтиже:cізге кешке кіруге бщлады!
